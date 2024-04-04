13c2cb22bf519fd25e2673cc860d11b8fb94002b
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-04 16:33:28 +0800

    Update ScriptPack.hx

--- source/funkin/backend/scripting/ScriptPack.hx
+++ source/funkin/backend/scripting/ScriptPack.hx
@@ -51,17 +51,6 @@ class ScriptPack extends Script {
 		return script;
 	}
 
-	public static function importScript1(path:String):Script {
-		var script = Script.create(Paths.script(path));
-		if (script is DummyScript) {
-			throw 'Script at ${path} does not exist.';
-			return null;
-		}
-		add(script);
-		script.load();
-		return script;
-	}
-
 	public override function call(func:String, ?parameters:Array<Dynamic>):Dynamic {
 		for(e in scripts)
 			if(e.active)
