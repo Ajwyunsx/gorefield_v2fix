f41975a3f46920b73af7677489cb3b1bb87f517f
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-04 13:02:50 +0800

    Update StoryMenuState.hx

--- source/funkin/menus/StoryMenuState.hx
+++ source/funkin/menus/StoryMenuState.hx
@@ -1856,7 +1856,6 @@ var codes:Map<String, Void -> Void> = [
 	"PEDAZO" => function() CodesFunctions.meme("pedazo gato tremendo")
 
 	// Youtubers english codes
-	"PENKARU" => CodesFunctions.penk;
 	"TAEYAI" => function() CodesFunctions.meme("t"),
 	"NIFFIRG" => function() CodesFunctions.meme("niffirgflumbo"),
 
